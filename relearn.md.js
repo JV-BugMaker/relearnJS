1073e7fc7a4724c8f1dc09eb79507c14d6671724
JV <gjw15546192557@gmail.com>
2016-07-11 23:48:30 +0800

    [+]修改后缀名

---
+++ relearn.md.js
@@ -0,0 +1,16 @@
+重新学习js--笔记
+概览：
+
+一、类型：
+
+	Number（数字）
+	String(字符串)
+	Boolean（布尔型）
+	Fcuntion（函数）
+	Object（对象）--undefined、null、Array 都是特殊对象
+	Symbol（第六版新加）符号
+	Error---？
+
+二、数字
+	
+	js采用IEEE 754 双精度64位格式
