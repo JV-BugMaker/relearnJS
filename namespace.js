531eb32cc8f372994e024713b54ba72193810425
JV <gjw15546192557@gmail.com>
2016-07-14 01:01:39 +0800

    [+]增加对js命名空间代码理解

---
+++ namespace.js
@@ -0,0 +1,29 @@
+var MYAPP = MYAPP || {};
+
+//创建子命名空间
+MYAPP.event = {};
+MYAPP.commonMethod = {
+  regExForName:"",
+  regExForPhone:"",
+  validateName:function(name){
+
+  },
+  validatePhone:function(phone){
+
+  }
+}
+//对象和方法一起声明
+
+MYAPP.event = {
+  addListener:function(el,type,callback){
+
+  },
+  removeListerner:function(el,type,callback){
+
+  },
+  getEvent:function(el,type,callback){
+
+  }
+}
+
+MYAPP.event.addListener("jv","shuaige",callback);
