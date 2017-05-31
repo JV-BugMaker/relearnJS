39a7b6a711c18c4105b213881069e5221c35ce04
JV-BugMaker <794241063@qq.com>
2017-05-31 22:52:30 +0800

    [*]两类函数调用this问题

--- web.js/closure.js
+++ web.js/closure.js
@@ -27,3 +27,25 @@ var a = {
 };
 
 a.b();
+// 在匿名函数中，以及直接通过函数名调用的函数中，this始终是window，除非通过apply或者call改变过值
+var name = "Im window";
+var obj = {
+	name:"Im obj",
+	aaa: function () {
+
+		console.log(this);// obj
+
+		function werw() {
+			// 这里的this始终指向window
+			console.log(this.name);
+		}
+
+		werw();// Im window 直接通过函数名调用的函数
+		return werw;
+	}
+};
+
+obj.aaa()();// window
+// 等同
+(obj.aaa())();// window
+
