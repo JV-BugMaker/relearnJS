23bec966cd780d2cf5c1a0563a9dba565d078cef
JV-BugMaker <794241063@qq.com>
2017-06-04 12:36:07 +0800

    [*]使用闭包进行惰性载入函数

--- web.js/lazy_load.js
+++ web.js/lazy_load.js
@@ -32,3 +32,32 @@ function createXHR()
 
     return createXHR();
 }
+
+//使用闭包进行处理
+var create = (function(){
+    if(typeof XMLHttpRequest != 'undefined'){
+        return function(){
+            return new XMLHttpRequest();
+        };
+    }else if(typeof ActiveXObject != 'undefined'){
+       return function(){
+            if(typeof arguments.callee.activeXString != 'string'){
+                var versions = ['MSXML2.XMLHttp.6.0','MSXML2.XMLHttp.3.0','MSXML2.XMLHttp'],i,len;
+                for(i=0,len=version.length;i<len;i++){
+                    try{
+                        new ActiveXObject(versions[i]);
+                        arguments.callee.activeXString = versions[i];
+                    }catch(error){
+                        
+                    }
+
+                }
+            }
+            return new ActiveXObject(arguments.callee.activeXString);
+        };
+    }else{
+        return function(){
+            throw new Error("No XHR object avilable");
+        };
+    }
+})();
